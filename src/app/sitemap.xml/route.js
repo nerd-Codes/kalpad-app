9dcde2b683ee0723926aec9ebb2a8863d2ae5b6c
Srijal Kumar <srijalgupta123@gmail.com>
2025-09-27 01:44:00 +0530

    added sitemap and robots.txt

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,9 +1,8 @@
 // src/app/sitemap.xml/route.js
-export const runtime = "nodejs"; // force Node runtime (Buffer etc. work here)
+export const runtime = "nodejs";
 
-export async function GET() {
+function buildSitemap() {
   const baseUrl = 'https://kalpad-app.vercel.app';
-
   const routes = ['/', '/sign-in', '/sign-up', '/reset-password'];
 
   const urls = routes.map(
@@ -16,15 +15,28 @@ export async function GET() {
       </url>`
   );
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${urls.join('')}
     </urlset>`;
+}
 
+export async function GET() {
+  const sitemap = buildSitemap();
   return new Response(sitemap, {
     headers: {
       "Content-Type": "application/xml; charset=utf-8",
-      // Node Buffer works now that runtime is forced to nodejs
+      "Content-Length": Buffer.byteLength(sitemap, "utf8").toString(),
+    },
+  });
+}
+
+// 👇 NEW
+export async function HEAD() {
+  const sitemap = buildSitemap();
+  return new Response(null, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
       "Content-Length": Buffer.byteLength(sitemap, "utf8").toString(),
     },
   });
